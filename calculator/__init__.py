0200292a43590b3cb779af52cd43d5cab04f468d
AkashDeore15 <akashdeore1999@gmail.com>
2025-02-11 15:53:38 -0500

    calculator part 2 commits

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,5 +1,32 @@
-def add(a,b):
-    return a + b
+from calculator.operations import add, subtract, multiply, divide
+from calculator.calculations import Calculations
 
-def subtract(a,b):
-    return a - b
+class Calculator:
+    def __init__(self):
+        self.calculations = []
+    
+    @staticmethod
+    def add(a, b):
+        calc = Calculations(a, b, add)
+        return calc.get_result()
+    
+    @staticmethod
+    def subtract(a, b):
+        calc = Calculations(a, b, subtract)
+        return calc.get_result()
+    
+    @staticmethod
+    def multiply(a, b):
+        calc = Calculations(a, b, multiply)
+        return calc.get_result()
+
+    @staticmethod
+    def divide(a, b):
+        calc = Calculations(a, b, divide)
+        return calc.get_result()
+
+    '''
+    @staticmethod
+    def get_calculations(self):
+        return self.calculations
+    '''

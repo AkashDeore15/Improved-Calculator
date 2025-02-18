a0d6fb2d6cd1d8cedc9476751bf09f6df032974c
AkashDeore15 <akashdeore1999@gmail.com>
2025-02-18 16:22:52 -0500

    updated return types in calculations file

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -7,28 +7,24 @@ from calculator.calculation import Calculation
 class Calculator:
     
     @staticmethod
-    def _get_calculations(a: Decimal, b: Decimal, operation: Callable) -> Decimal:
+    def _get_calculations(a: Decimal, b: Decimal, operation: Callable[[Decimal, Decimal], Decimal]) -> Decimal:
         """Create and Perform a calculation, then return the result."""
         calculation = Calculation.create(a, b, operation)
         Calculations.add_calculation(calculation)
-        return calculation
+        return calculation.get_result()
     
     @staticmethod
     def add(a:Decimal, b:Decimal) -> Decimal:
-        calculation = Calculator._get_calculations(a, b, add)
-        return calculation.get_result()
+        return Calculator._get_calculations(a, b, add)
     
     @staticmethod
     def subtract(a:Decimal, b:Decimal) -> Decimal:
-        calculation = Calculator._get_calculations(a, b, subtract)
-        return calculation.get_result()
+        return Calculator._get_calculations(a, b, subtract)
     
     @staticmethod
     def multiply(a:Decimal, b:Decimal) -> Decimal:
-        calculation = Calculator._get_calculations(a, b, multiply)
-        return calculation.get_result()
+        return Calculator._get_calculations(a, b, multiply)
 
     @staticmethod
     def divide(a:Decimal, b:Decimal) -> Decimal:
-        calculation = Calculator._get_calculations(a, b, divide)
-        return calculation.get_result()
+        return Calculator._get_calculations(a, b, divide)
--- calculator/calculations.py
+++ calculator/calculations.py
@@ -10,7 +10,7 @@ class Calculations:
         cls.history.append(calculation)
 
     @classmethod
-    def get_history(cls):
+    def get_history(cls) -> List[Calculation]:
         """Return the calculation history."""
         return cls.history
 
@@ -20,17 +20,17 @@ class Calculations:
         cls.history.clear()
     
     @classmethod
-    def get_last_calculation(cls):
+    def get_last_calculation(cls) -> Calculation:
         """Return the last calculation."""
         return cls.history[-1] if cls.history else None
 
     @classmethod
-    def get_last_n_calculations(cls, n: int):
+    def get_last_n_calculations(cls, n: int) -> List[Calculation]:
         """Return the last n calculations."""
         return cls.history[-n:] if cls.history else None
 
     @classmethod
-    def get_first_n_calculations(cls, n: int):
+    def get_first_n_calculations(cls, n: int) -> List[Calculation]:
         """Return the first n calculations."""
         return cls.history[:n] if cls.history else None
     
